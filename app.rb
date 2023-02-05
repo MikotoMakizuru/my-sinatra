6e873f4e2aa863f512535a04ca45cac69b0a7902
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-02-05 17:54:27 +0900

    メモの内容をJSON形式でJSONファイルへ追加したいがわからない

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
-require 'pry'
+require 'ulid'
 
-@memos = []
+FILE_PATH = "memos/memos.json"
+
+# memos = []
 
 get '/' do
 	redirect '/memos'
 end
 
 get '/memos' do
+	Dir.glob(FILE_PATH) do |file|
+		file_parse = JSON.parse (File.open(file).read)
+		file_parse = JSON.parse file_parse
+		memos << file_parse
+		@memos = memos
+	end
 	erb :index
 end
 
@@ -18,15 +26,59 @@ get '/memos/new' do
 end
 
 post '/' do
-	@memos << params[:title]
-	binding.pry
-	erb :index
+	memo = {
+		'memo_id'=>ULID.generate,
+		'title'=>params[:title],
+		'content'=>params[:content]
+	}
+	memo_json = JSON.generate(memo)
+	File.open(FILE_PATH, 'a') do |file|
+		str = JSON.dump(memo_json, file)
+	end
+	redirect '/memos'
 end
 
-get '/memos/show' do
+get '/memos/:memo_id' do |memo_id|
+	# @memos = []
+	memos.each do |memo|
+		if memo_id == memo[:memo_id]
+			@memos << memo
+		end
+	end
 	erb :show
 end
 
-get '/memos/edit' do
+get '/memos/:memo_id/edit' do |memo_id|
+	# @memos = []
+	memos.each do |memo|
+		if memo_id == memo[:memo_id]
+			@memos << memo
+		end
+	end
 	erb :edit
+end
+
+patch '/memos/:memo_id' do |memo_id|
+	title = params[:title]
+	content = params[:content]
+
+	memos.each do |memo|
+		if memo_id == memo[:memo_id]
+			memo[:title] = title
+			memo[:content] = content
+		end
+	end
+	redirect "/memos/#{memo_id}"
+end
+
+delete '/memos/:memo_id' do |memo_id|
+  memos.each do |memo|
+		if memo_id == memo[:memo_id]
+			memo.delete(:memo_id)
+			memo.delete(:title)
+			memo.delete(:content)
+			@memos = memos
+			redirect '/memos'
+		end
+	end
 end
