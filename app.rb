e6eaadd95e2558980bf37990c950dc09221e7d96
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-02-09 20:50:40 +0900

    メモの追加, 詳細閲覧, 編集, 削除機能を実装

--- app.rb
+++ app.rb
@@ -1,84 +1,95 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'ulid'
+require 'erb'
 
-FILE_PATH = "memos/memos.json"
+FILE_PATH = 'memos/memos.json'
 
-memos = []
+helpers do
+  def h(text)
+    escape_html(text)
+  end
+end
 
 get '/' do
-	redirect '/memos'
+  redirect '/memos'
 end
 
 get '/memos' do
-	Dir.glob(FILE_PATH) do |file|
-		file_parse = JSON.parse (File.open(file).read)
-		file_parse = JSON.parse file_parse
-		memos << file_parse
-		@memos = memos
-	end
-	erb :index
+  load_memos = File.read(FILE_PATH) { |file| JSON.parse(file) }
+  @memos = JSON.parse load_memos
+  erb :index
 end
 
 get '/memos/new' do
-	erb :new
+  erb :new
 end
 
 post '/' do
-	memo = {
-		'memo_id'=>ULID.generate,
-		'title'=>params[:title],
-		'content'=>params[:content]
-	}
-	memo_json = JSON.pretty_generate(memo)
-	File.open(FILE_PATH, 'a') do |file|
-		str = JSON.dump(memo_json, file)
+  memo = {
+          'memo_id' => ULID.generate,
+          'title'   => "#{h(params[:title])}",
+          'content' => "#{h(params[:content])}"
+				}
+	File.open(FILE_PATH) do |file|
+		load_memos = JSON.load(file)
+		load_memos << memo
+		File.open(FILE_PATH, "w") { |file| JSON.dump(load_memos, file) }
 	end
 	redirect '/memos'
 end
 
 get '/memos/:memo_id' do |memo_id|
-	# @memos = []
+	memos = File.open(FILE_PATH) { |file| JSON.load(file) }
 	memos.each do |memo|
-		if memo_id == memo[:memo_id]
-			@memos << memo
+		if memo_id == memo['memo_id']
+			@memo_id = memo['memo_id']
+			@title = memo['title']
+			@content = memo['content']
 		end
 	end
 	erb :show
 end
 
 get '/memos/:memo_id/edit' do |memo_id|
-	# @memos = []
+	memos = File.open(FILE_PATH) { |file| JSON.load(file) }
 	memos.each do |memo|
-		if memo_id == memo[:memo_id]
-			@memos << memo
+		if memo_id == memo['memo_id']
+			@memo_id = memo['memo_id']
+			@title = memo['title']
+			@content = memo['content']
 		end
 	end
 	erb :edit
 end
 
 patch '/memos/:memo_id' do |memo_id|
-	title = params[:title]
-	content = params[:content]
+	title = "#{h(params[:title])}"
+	content = "#{h(params[:content])}"
 
+	memos = File.open(FILE_PATH) { |file| JSON.load(file) }
 	memos.each do |memo|
-		if memo_id == memo[:memo_id]
-			memo[:title] = title
-			memo[:content] = content
+		if memo_id == memo['memo_id']
+
+			memo['title'] = title
+			memo['content'] = content
+			File.open(FILE_PATH, "w") { |file| JSON.dump(memos, file) }
 		end
 	end
 	redirect "/memos/#{memo_id}"
 end
 
 delete '/memos/:memo_id' do |memo_id|
+	memos = File.open(FILE_PATH) { |file| JSON.load(file) }
   memos.each do |memo|
-		if memo_id == memo[:memo_id]
-			memo.delete(:memo_id)
-			memo.delete(:title)
-			memo.delete(:content)
-			@memos = memos
-			redirect '/memos'
+		if memo_id == memo['memo_id']
+			memos.delete(memo)
+			File.open(FILE_PATH, "w") { |file| JSON.dump(memos, file) }
 		end
 	end
+	erb :index
+	redirect '/memos'
 end
